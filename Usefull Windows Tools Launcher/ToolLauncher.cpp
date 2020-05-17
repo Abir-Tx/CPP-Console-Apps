636947b9e952f12a63e1f153bb0e3a636f910016
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-05-17 12:20:44 +0600

    Created the basic app

--- Usefull Windows Tools Launcher/ToolLauncher.cpp
+++ Usefull Windows Tools Launcher/ToolLauncher.cpp
@@ -2,18 +2,79 @@
 /* It is a CMD tool which will launch secret and usefull windows 10 apps/tools. */
 #include <iostream>
 #include <iomanip>
+#include <vector>
 using namespace std;
+
+//Global var
+int menuChoice;
+
+//Func
+void heading()
+{
+  cout<<right<<setw(85)<<setfill(' ')<<"Windows Tool Launcher"<<endl<<endl;
+}
+
+void menu()
+{
+  vector <string> menu;
+  menu={"Launcher","Theme","Exit"};
+
+  for (int i=0;i<menu.size();i++)
+  {
+    cout<<i+1<<". "<<menu[i]<<endl;
+  }
+
+  cout<<"Enter your choice: ";
+  cin>>menuChoice;
+}
+
+//classes
+class launcher
+{
+  protected:
+
+
+  public:
+  void internet()
+  {
+    system("cls");
+    cout<<right<<setw(85)<<setfill('_')<<"Internet Section"<<endl<<endl;
+
+    vector <string> menu;
+    menu={"Google"};
+
+    for (int i = 0; i<menu.size(); i++)
+    {
+      cout<<i+1<<". "<<menu[i]<<endl;
+    }
+    cout<<"Enter your choice: ";
+    int choice;
+    cin>>choice;
+
+    switch (choice)
+    {
+    case 1:
+      system("start www.google.com");
+      break;
+    
+    default:
+    cout<<"Invalid Input"<<endl;
+      break;
+    }
+  }
+};
 
 int main()
 {
     system("title Widnows Tool Launcher");
-    cout<<right<<setw(85)<<setfill(' ')<<"Windows Tool Launcher"<<endl<<endl;
-    cout << showbase << uppercase;
-  cout << uppercase <<"Google" << '\n';
-  cout << nouppercase << 77 <<"Google"<< '\n';
+    heading();
+    menu();
+    if (menuChoice==1)
+    {
+      launcher sec1;
+      sec1.internet();
+    }
 
-   std::cout.flags ( std::ios::right | std::ios::showbase );
-  std::cout.width (50);
-  std::cout << 100 << '\n';
+
     system("pause");
 }
